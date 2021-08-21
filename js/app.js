cd9fe9e97c6028cdee51a629d1108dfd155f993b
himel <himelkhan290@gmail.com>
2021-08-21 15:10:56 +0600

    finish

--- js/app.js
+++ js/app.js
@@ -1,75 +1,98 @@
 // function for per items cost
  function itemCost(id, number){
-    const peviousMemoryPrice = document.getElementById(id + '-cost');
-    peviousMemoryPrice.innerText = number;
-    const newcost = parseInt(peviousMemoryPrice.innerText);   
+    const itemPrice = document.getElementById(id + '-cost');
+    itemPrice.innerText = number;
+    const itemnewPrice = parseInt(itemPrice.innerText);   
  }
 
-// function for memory
+// function for memory item
 function memory(id){
     return document.getElementById(id + '-memory');
 }
 
-// function for storage
+// function for storage item
 function storage(id){
     return document.getElementById(id + '-storage');
 }
 
-// function for delivery
+// function for delivery item
 function delivery(id){
     return document.getElementById(id + '-delivery');
+}
+// function for footer total 
+function discountTotal(){
+    return document.getElementById('discount-total');
+}
+    
+// function for item total cost
+function itemTotalCost(id){
+    return document.getElementById(id).innerText;
 }
 
 // function for total price 
 function totalPrice(){
-    const bestPrice = document.getElementById('best-price').innerText;
-    const memoryTotal = document.getElementById('memory-cost').innerText;
-    const storageTotal = document.getElementById('storage-cost').innerText;
-    const deliveryTotal = document.getElementById('delivery-cost').innerText;
+    const bestPrice = itemTotalCost('best-price');
+    const memoryTotal = itemTotalCost('memory-cost');
+    const storageTotal = itemTotalCost('storage-cost');
+    const deliveryTotal = itemTotalCost('delivery-cost');
     const totalCost = document.getElementById('total-cost');
     totalCost.innerText = parseInt(bestPrice) + parseInt(memoryTotal) + parseInt(storageTotal) + parseInt(deliveryTotal);
+
+    // function for footer total 
+    discountTotal().innerText = totalCost.innerText; 
 }
 
 // event handler for memory 
-const firstMemoryCheck = memory('first');
-const secondMemoryCheck = memory('second');
-firstMemoryCheck.addEventListener('click',function(){
+const firstMemoryItem = memory('first');
+const secondMemoryItem = memory('second');
+firstMemoryItem.addEventListener('click', function(){
     itemCost('memory', 0);
     totalPrice();
     
 })
-secondMemoryCheck.addEventListener('click',function(){
+secondMemoryItem.addEventListener('click', function(){
     itemCost('memory', 180);
     totalPrice();
 })
 
 // event handler for storage 
-const firstStorageCheck = storage('first');
-const secondStorageCheck = storage('second');
-const thirdStorageCheck = storage('third');
-firstStorageCheck.addEventListener('click',function(){
+const firstStorageItem = storage('first');
+const secondStorageItem = storage('second');
+const thirdStorageItem = storage('third');
+firstStorageItem.addEventListener('click', function(){
     itemCost('storage', 0);
     totalPrice();
 })
-secondStorageCheck.addEventListener('click',function(){
+secondStorageItem.addEventListener('click', function(){
     itemCost('storage', 100);
     totalPrice();
 })
-thirdStorageCheck.addEventListener('click',function(){
+thirdStorageItem.addEventListener('click', function(){
     itemCost('storage', 180);
     totalPrice();
 })
 
 // event handler for delivery 
-const firstDelivery = delivery('first');
-const secondDelivery = delivery('second');
-firstDelivery.addEventListener('click',function(){
+const firstDeliveryPackage = delivery('first');
+const secondDeliveryPackage = delivery('second');
+firstDeliveryPackage.addEventListener('click', function(){
     itemCost('delivery', 0);
     totalPrice();
 })
 
-secondDelivery.addEventListener('click',function(){
+secondDeliveryPackage.addEventListener('click', function(){
     itemCost('delivery', 20);
     totalPrice();
 })
 
+// event handler for pomo code
+const pomoBtn = document.getElementById('pomo-btn');
+pomoBtn.addEventListener('click', function(){
+    const pomoCode = document.getElementById('pomo-code');
+    const discountText = discountTotal().innerText;
+    const discountAmount = parseInt(discountText);
+    if(pomoCode.value == 'stevekaku'){
+        discountTotal().innerText = discountAmount - discountAmount * 0.2;
+    }
+    pomoCode.value = '';
+})
